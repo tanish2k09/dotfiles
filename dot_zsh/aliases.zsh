9ae5df3aed4a00c7eb5d07e36d7beb618668d9e3
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-12 22:16:34 -0700

    Add ZSH setup

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,8 @@
+ONEDRIVE=~/Library/CloudStorage/OneDrive-Personal/
+
+hash -d prj=~/Stuff/Projects/
+hash -d onedrive=$ONEDRIVE
+hash -d sem=$ONEDRIVE/Work/SFU/15\ -\ Spring\ 2024/
+
+# Use colored LS command by default 
+alias ls='ls --color=auto'
---
+++ dot_zsh/plugins.zsh
@@ -0,0 +1,8 @@
+PLUGDIR=$ZSHXT/plugins
+
+source $PLUGDIR/powerlevel10k/powerlevel10k.zsh-theme
+source $PLUGDIR/auto-suggestions/zsh-autosuggestions.zsh
+source $PLUGDIR/auto-complete/zsh-autocomplete.plugin.zsh
+source $PLUGDIR/highlighting/F-Sy-H.plugin.zsh
+source $PLUGDIR/dirhistory.zsh
+source $PLUGDIR/jsontools.zsh
