63373dc6381cfdf23ee02ca28b5389daea3445de
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-20 01:22:01 -0700

    Update ZSH and iTerm preferences

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -2,7 +2,10 @@ ONEDRIVE=~/Library/CloudStorage/OneDrive-Personal/
 
 hash -d prj=~/Stuff/Projects/
 hash -d onedrive=$ONEDRIVE
-hash -d sem=$ONEDRIVE/Work/SFU/15\ -\ Spring\ 2024/
+hash -d sem=$ONEDRIVE/Work/SFU/16\ -\ Summer\ 2024/
 
 # Use colored LS command by default 
 alias ls='ls --color=auto'
+
+# Shortcut to edit nvim config directly
+alias nvimc='nvim ~/.config/nvim/'
--- dot_zsh/opts.zsh
+++ dot_zsh/opts.zsh
@@ -1,1 +1,5 @@
 setopt AUTO_CD 
+export VISUAL=nvim
+export EDITOR=nvim
+export MANPAGER='nvim +Man!'
+export MANWIDTH=999
