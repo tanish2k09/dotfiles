6bac5b79c05bd3f6c7e98018eea760b952f8bd3a
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-23 04:29:28 -0700

    NVIM: Add blank indent-line

---
+++ dot_config/nvim/lua/tanish/plugins/ui/indent-line.lua
@@ -0,0 +1,30 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "│",
+				-- tab_char overrides the tab whitespace character in list chars vim opt
+				-- tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					-- "lazyterm",
+				},
+			},
+		},
+	},
+}
