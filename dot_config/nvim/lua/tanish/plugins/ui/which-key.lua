b1b92074072cd3388ffa337546f0cc82b781bce4
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-23 04:30:11 -0700

    NVIM: Update/Add Which-key registered keys

--- dot_config/nvim/lua/tanish/plugins/ui/which-key.lua
+++ dot_config/nvim/lua/tanish/plugins/ui/which-key.lua
@@ -8,16 +8,19 @@ return { -- Useful plugin to show you pending keybinds.
 		require("which-key").register({
 			["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>cr"] = { name = "[C]ode [R]adical", _ = "which_key_ignore" },
+			["<leader>cr"] = { name = "[R]adical", _ = "which_key_ignore" },
 			["<leader>d"] = { name = "[D]ebug", _ = "which_key_ignore" },
 			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]oto", _ = "which_key_ignore" },
+			["<leader>g"] = { name = "[G]oto/[G]it", _ = "which_key_ignore" },
 			["<leader>l"] = { name = "[L]SP", _ = "which_key_ignore" },
+			["<leader>p"] = "which_key_ignore",
 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]rouble", _ = "which_key_ignore" },
+			["<leader>t"] = { name = "[T]rouble/[T]erminal", _ = "which_key_ignore" },
 			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
 			["<leader>s"] = { name = "[S]ession", _ = "which_key_ignore" },
-			["<leader>u"] = { name = "UI", _ = "which_key_ignore" },
+			["<leader>u"] = { name = "[U]I", _ = "which_key_ignore" },
+			["<leader>un"] = { name = "[N]otifications", _ = "which_key_ignore" },
+			["<localLeader>l"] = { name = "[V]imtex" },
 		})
 		-- visual mode
 		require("which-key").register({
