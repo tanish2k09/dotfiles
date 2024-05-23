31ce4dca5ef875611c143036c0f3730c0752d0fe
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-23 04:32:36 -0700

    NVIM: Add vimtex

---
+++ dot_config/nvim/lua/tanish/plugins/provisions/vimtex.lua
@@ -0,0 +1,10 @@
+return {
+	"lervag/vimtex",
+	lazy = false, -- we don't want to lazy load VimTeX
+	-- tag = "v2.15", -- uncomment to pin to a specific release
+	init = function()
+		vim.g.vimtex_view_method = "sioyek"
+		vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+	end,
+}
