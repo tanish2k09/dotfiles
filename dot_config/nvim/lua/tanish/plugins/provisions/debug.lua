a8613bcbd8350829de781e4228757ee721ca7172
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-23 04:31:36 -0700

    NVIM: Attempt to fix the dapui config error on startup

--- dot_config/nvim/lua/tanish/plugins/provisions/debug.lua
+++ dot_config/nvim/lua/tanish/plugins/provisions/debug.lua
@@ -6,10 +6,42 @@ return {
 		"rcarriga/nvim-dap-ui",
 		-- Add your own debuggers here
 		-- "leoluz/nvim-dap-go",
+		"jbyuki/one-small-step-for-vimkind",
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
+
+		dap.adapters.nlua = function(callback, conf)
+			local adapter = {
+				type = "server",
+				host = conf.host or "127.0.0.1",
+				port = conf.port or 8086,
+			}
+			if conf.start_neovim then
+				local dap_run = dap.run
+				dap.run = function(c)
+					adapter.port = c.port
+					adapter.host = c.host
+				end
+				require("osv").run_this()
+				dap.run = dap_run
+			end
+			callback(adapter)
+		end
+		dap.configurations.lua = {
+			{
+				type = "nlua",
+				request = "attach",
+				name = "Run this file",
+				start_neovim = {},
+			},
+			{
+				type = "nlua",
+				request = "attach",
+				name = "Attach to running Neovim instance (port = 8086)",
+				port = 8086,
+			},
+		}
 
 		-- Basic debugging keymaps, feel free to change to your liking!
 		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
@@ -20,12 +52,5 @@ return {
 		vim.keymap.set("n", "<leader>dB", function()
 			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 		end, { desc = "Debug: Set Breakpoint" })
-
-		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
-
-		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		dap.listeners.before.event_exited["dapui_config"] = dapui.close
 	end,
 }
--- dot_config/nvim/lua/tanish/plugins/ui/nvim-dap-ui.lua
+++ dot_config/nvim/lua/tanish/plugins/ui/nvim-dap-ui.lua
@@ -26,4 +26,35 @@ return {
 			},
 		},
 	},
+	keys = {
+		{
+			"<leader>du",
+			function()
+				require("dapui").toggle({})
+			end,
+			desc = "[D]ebug: Toggle DAP [U]I",
+		},
+		{
+			"<leader>de",
+			function()
+				require("dapui").eval()
+			end,
+			desc = "[D]ebug: DAP Eval",
+			mode = { "n", "v" },
+		},
+	},
+	config = function(_, opts)
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup(opts)
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open({})
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close({})
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close({})
+		end
+	end,
 }
