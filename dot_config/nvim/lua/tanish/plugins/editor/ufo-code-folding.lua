a49f16843333c3341d6b475ce09fc7ea7baf9526
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-24 07:19:07 -0700

    NVIM: UFO: Hide the fold column numbers

--- dot_config/nvim/lua/tanish/plugins/editor/ufo-code-folding.lua
+++ dot_config/nvim/lua/tanish/plugins/editor/ufo-code-folding.lua
@@ -2,7 +2,7 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async", "neovim/nvim-lspconfig" },
 	config = function()
-		vim.o.foldcolumn = "3" -- '0' is not bad
+		vim.o.foldcolumn = "0" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
