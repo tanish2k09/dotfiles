11a10e71fd08096ce6f76217983fbab254ea91f8
Tanish Manku <tanish2k09.dev@gmail.com>
2024-05-26 20:32:31 -0700

    NVIM: Run conform on init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,6 @@
-require 'tanish.config.opts'
-require 'tanish.config.keymaps'
-require 'tanish.config.autocmd'
-require 'tanish.config.lazy'
+require("tanish.config.opts")
+require("tanish.config.keymaps")
+require("tanish.config.autocmd")
+require("tanish.config.lazy")
 
 -- vim: ts=2 sts=2 sw=2 et
