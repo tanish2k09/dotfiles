a93e3a16267fc8195bafb86bc3d8725f51301f99
Tanish Manku <tanish2k09.dev@gmail.com>
2024-07-06 04:08:56 -0700

    NVIM: Fix autotags

--- dot_config/nvim/lua/tanish/plugins/code/autotags.lua
+++ dot_config/nvim/lua/tanish/plugins/code/autotags.lua
@@ -1,4 +1,8 @@
 return {
 	"windwp/nvim-ts-autotag",
-	event = "InsertEnter",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		require("nvim-ts-autotag").setup()
+	end,
+	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 }
