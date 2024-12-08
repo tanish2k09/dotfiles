ea5c8b764921a0b27b0a1c39d303bb3aaa84ed34
Tanish Manku <tanish2k09.dev@gmail.com>
2024-12-07 18:16:45 -0800

    NVIM: Copilot-cmp: Do nil check

--- dot_config/nvim/lua/tanish/plugins/provisions/copilot-cmp.lua
+++ dot_config/nvim/lua/tanish/plugins/provisions/copilot-cmp.lua
@@ -14,7 +14,7 @@ return {
 		vim.api.nvim_create_autocmd("LspAttach", {
 			callback = function(args)
 				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				if client.name == "copilot" then
+				if client and client.name == "copilot" then
 					copilot_cmp._on_insert_enter({})
 				end
 			end,
