9786efd266365c41d9e2f9054c56c36494e017f2
Tanish Manku <tanish2k09.dev@gmail.com>
2024-12-09 17:44:51 -0800

    NVIM: Themes: Add some new themes

--- dot_config/nvim/lua/tanish/themes/loader.lua
+++ dot_config/nvim/lua/tanish/themes/loader.lua
@@ -12,4 +12,17 @@ return {
 	{ "AhmedAbdulrahman/vim-aylin" }, -- lazy
 	{ "tyrannicaltoucan/vim-deep-space" }, -- lazy
 	{ "miikanissi/modus-themes.nvim" }, -- lazy
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+	},
+	{ "ribru17/bamboo.nvim" },
+	{ "muellan/am-colors" },
+	{ "irhl/flowershop.vim" },
+	{ "mbeach42/antergos-vm-sp4-rice" },
+	{ "projekt0n/caret.nvim" },
+	{ "smoka7/celeste-nvim" },
 }
