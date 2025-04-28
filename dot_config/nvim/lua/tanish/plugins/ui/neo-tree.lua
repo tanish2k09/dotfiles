365fadbe179b51c5cb54d5da70d2ef742a73dc71
Tanish Manku <tanish2k09.dev@gmail.com>
2025-04-28 00:14:01 -0700

    NVIM: Neotree bind_to_cwd can be specified

--- dot_config/nvim/lua/tanish/plugins/ui/neo-tree.lua
+++ dot_config/nvim/lua/tanish/plugins/ui/neo-tree.lua
@@ -62,6 +62,7 @@ return {
 		},
 		open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
 		filesystem = {
+			bind_to_cwd = false,
 			follow_current_file = { enabled = false },
 			use_libuv_file_watcher = true,
 			hijack_netrw_behaviour = "open_current",
