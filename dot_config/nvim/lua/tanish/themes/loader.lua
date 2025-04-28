3f90de227c1454ee27559a9a718aa9305728876a
Tanish Manku <tanish2k09.dev@gmail.com>
2025-04-28 00:13:16 -0700

    NVIM: Add catppuccin and some theme extras

--- dot_config/nvim/lua/tanish/themes/loader.lua
+++ dot_config/nvim/lua/tanish/themes/loader.lua
@@ -1,12 +1,39 @@
 return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			transparent_background = true,
+			show_end_of_buffer = true,
+		},
+	},
 	{ "EdenEast/nightfox.nvim" }, -- lazy
 	{
 		"embark-theme/vim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme embark]])
+			-- This function clears the background for several highlight groups
+			local function set_transparency()
+				vim.cmd("hi Normal guibg=NONE ctermbg=NONE") -- Main background
+				vim.cmd("hi NonText guibg=NONE ctermbg=NONE") -- Text area padding
+				vim.cmd("hi VertSplit guibg=NONE ctermbg=NONE") -- Vertical split lines
+				vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE") -- Status line
+				vim.cmd("hi StatusLineNC guibg=NONE ctermbg=NONE") -- Inactive status line
+				vim.cmd("hi LineNr guibg=NONE ctermbg=NONE") -- Line numbers
+				vim.cmd("hi CursorLine guibg=NONE ctermbg=NONE") -- Cursor line background
+				vim.cmd("hi Pmenu guibg=NONE ctermbg=NONE") -- Popup menu
+				vim.cmd("hi PmenuSel guibg=NONE ctermbg=NONE") -- Selected popup menu item
+				vim.cmd("hi Folded guibg=NONE ctermbg=NONE") -- Folded text
+				vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE") -- End of buffer line
+				vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE") -- Normal background for non-current windows
+			end
+
+			local theme = "embark"
+			vim.cmd([[colorscheme ]] .. theme)
+			-- set_transparency()
 		end,
 	},
 	{ "AhmedAbdulrahman/vim-aylin" }, -- lazy
